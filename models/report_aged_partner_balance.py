51c3548357b67ea92d994c2a8fe36f5059d1b009
rquintanam <itgeopanama@gmail.com>
2019-08-04 18:05:30 -0500

    Traducido al Español
    Traducido al Español

--- models/report_aged_partner_balance.py
+++ models/report_aged_partner_balance.py
@@ -7,8 +7,8 @@ class AgedTrialBalanceBillwise(models.TransientModel):
     _inherit = 'account.aged.trial.balance'
     _description = 'Account Aged Trial balance Report Bill-wise'
 
-    report_type = fields.Selection([('bill', 'Bill-wise'), ('customer', 'Customer-wise')], default='customer',
-                                   string="Report Type")
+    report_type = fields.Selection([('bill', 'Detallado'), ('customer', 'Por Cliente')], default='customer',
+                                   string="Tipo de Reporte")
 
     def _print_report(self, data):
         data['form']['report_type'] = self.report_type
